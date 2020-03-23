7bda092a70244564b5b48c37e00f45feea027889
Matthias K <kubbixx@web.de>
2020-03-23 08:59:32 +0100

    completed submit method

--- welcomeform.py
+++ welcomeform.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import passwordmanager as pwm
 import sqlite3
 
@@ -12,13 +13,13 @@ class welcome:
         self.master.title("Log in")
 
         # labels for entry forms
-        tk.Label(self.master, text="Username").grid(row=0)
-        tk.Label(self.master, text="Password").grid(row=1)
+        ttk.Label(self.master, text="Username").grid(row=0)
+        ttk.Label(self.master, text="Password").grid(row=1)
 
         # entry forms
-        self.entry_username = tk.Entry(self.master)
+        self.entry_username = ttk.Entry(self.master)
         self.entry_username.grid(row=0, column=1)
-        self.entry_password = tk.Entry(self.master, show="*")
+        self.entry_password = ttk.Entry(self.master, show="*")
         self.entry_password.grid(row=1, column=1)
 
         # Label for user feedback such as "username does not exist"
@@ -26,12 +27,12 @@ class welcome:
         self.feedback.grid(row=2)
 
         # button to submit entry
-        self.button_submit = tk.Button(self.master, text='Submit',
+        self.button_submit = ttk.Button(self.master, text='Submit',
         width=25, command=self.submit)
         self.button_submit.grid(row=3, column=0)
 
         # button to create new account
-        self.button_create = tk.Button(self.master, text='Create Account',
+        self.button_create = ttk.Button(self.master, text='Create Account',
         width=25, command=self.create_account)
         self.button_create.grid(row=3, column=1)
 
@@ -41,7 +42,7 @@ class welcome:
     # function to create sqlite-File if not existing and write the account
     # information the user submits into that file
     def create_account(self):
-        print("create account")
+        self.feedback["text"] = ""
 
         # get informations from entries
         self.username = self.entry_username.get()
@@ -56,27 +57,56 @@ class welcome:
                         username TEXT PRIMARY KEY, \
                         password TEXT)")
 
-        # insert information from entries into table
-        cursor.execute("INSERT INTO users(username, password) VALUES (?, ?)",
-                      (self.username, self.password))
-
+        try:
+            # insert information from entries into table
+            cursor.execute("INSERT INTO users(username, password) VALUES (?, ?)",
+                          (self.username, self.password))
+        except:
+            self.feedback["text"] = "username already exists"
         # commit changes to table and close connection
         connection.commit()
         connection.close()
 
-        # creates instance of manager an gives username for identification
-        pwm.manager(self.username)
+        # if account could not be created then dont open manager
+        if not self.feedback["text"] == "username already exists":
+            # creates instance of manager an gives username for identification
+            pwm.manager(self.username)
 
     # function to check weather the submitted informations match with existing
     # account informations or not, needs also to start the the password manager
     # via calling another class for that window
     def submit(self):
+        # get informations from entries
         self.username = self.entry_username.get()
         self.password = self.entry_password.get()
         print(self.username, self.password)
 
-        # creates instance of manager an gives username for identification
-        pwm.manager(self.username)
+        # connect to database and create instance of cursor
+        connection = sqlite3.connect("users.db")
+        cursor = connection.cursor()
+
+        # select db entry where username is equal to typed in username
+        cursor.execute("SELECT * FROM users WHERE username =(?)", (self.username,))
+
+        # get entry as strings
+        rows = cursor.fetchall()
+        try:
+            print(rows[0][1])
+            self.feedback["text"] = ""
+        except:
+            self.feedback["text"] = "username does not exist"
+
+        # close connection
+        connection.close()
+
+        # check if password fits to username
+        if rows[0][1] == self.password:
+            # creates instance of manager an gives username for identification
+            pwm.manager(self.username)
+            self.feedback["text"] = ""
+        else:
+            self.feedback["text"] = "incorrect password"
+
 
 
 if __name__ == '__main__':
